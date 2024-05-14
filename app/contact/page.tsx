c32e54c0f057c489259bd761549d8754930d62b3
iraniangunner <fateh19950@hotmail.com>
2024-05-14 21:57:08 +0330

    contact page changed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -26,13 +26,13 @@ export default function Contact() {
                 /
               </span>
             </li>
-            <li className="flex items-center font-iransans text-lg antialiased font-normal leading-normal  text-blue-gray-900">
+            <li className="flex items-center font-iransans text-lg antialiased font-normal leading-normal text-blue-gray-900">
               <span>تماس با ما</span>
             </li>
           </ol>
         </nav>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 pt-[50px] pb-[20px] lg:pt-[60px] lg:pb-[30px] px-10 lg:px-30 xl:px-40 2xl:px-80 mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 pt-[50px] pb-[20px] lg:pt-[60px] lg:pb-[30px] px-10 lg:px-30 xl:px-40 mx-auto">
         <div className="relative group flex items-center py-5 px-10 lg:py-[20px] lg:px-[30px] 2xl:py-[25px] shadow-[0_5px_20px_0_rgba(0,0,0,0.15)] overflow-hidden bg-white hover:shadow-[0_5px_60px_0_rgba(0,0,0,0.15)] hover:translate-y-[-10px] transition-all duration-[0.8s]">
           <FaPhone className="absolute text-[70px] left-[10px] bottom-[5px] leading-[1px] font-[700] text-[#000] transition-all duration-[0.8s] opacity-[0.05] group-hover:scale-[2]" />
           <FaPhone size={45} className="ml-[20px]" color="#ffa500" />
@@ -75,7 +75,7 @@ export default function Contact() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 pt-[50px] pb-[20px] lg:pt-[60px] lg:pb-[30px] px-10 lg:px-30 xl:px-40 2xl:px-60 mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 pt-[50px] pb-[20px] lg:pt-[60px] lg:pb-[30px] px-10 lg:px-30 xl:px-40 mx-auto">
         <div className="py-[40px] px-[45px] border border-solid border-[#fea925] w-[80%] mr-auto">
           <div className="text-center">
             <h2 className="mb-[10px] text-[25px] font-bold">
