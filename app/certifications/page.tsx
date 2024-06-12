07caab4f1d4646d5b0db90780d5e4edaa511ef06
fateh moonesi <f.moonesi@jsk.local>
2024-06-12 10:48:51 +0330

    Certification carousel changed

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -1,13 +1,5 @@
-import { CertificationsGallery } from "@/components/carousel/certificationGallery";
-import { SwiperCarousel } from "@/components/carousel/swiperModalCarousel";
+import { CertificationsCarousel } from "@/components/carousel/CertificationCarousel";
 
 export default function Certifications() {
-  return (
-    // <div className="min-h-[50vh]">
-    //   <h1>Certifications</h1>
-
-      // <CertificationsGallery />
-      <SwiperCarousel/>
-    // </div>
-  );
+  return <CertificationsCarousel />;
 }
---
+++ components/carousel/CertificationCarousel.tsx
@@ -0,0 +1,113 @@
+"use client";
+import React, { useState } from "react";
+import { Autoplay, Navigation, EffectFade } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import Modal from "react-responsive-modal";
+import 'react-responsive-modal/styles.css';
+
+const items = [
+  {
+    id: 1,
+    src: "https://swiperjs.com/demos/images/nature-2.jpg",
+    alt: "First slide",
+  },
+  {
+    id: 2,
+    src: "https://swiperjs.com/demos/images/nature-3.jpg",
+    alt: "Second slide",
+  },
+  {
+    id: 3,
+    src: "https://swiperjs.com/demos/images/nature-4.jpg",
+    alt: "Third slide",
+  },
+  {
+    id: 4,
+    src: "https://swiperjs.com/demos/images/nature-5.jpg",
+    alt: "Third slide",
+  },
+];
+
+export function CertificationsCarousel() {
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+  //   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  const handleOpen = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+    setOpen(true);
+  };
+
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div className="min-h-[100vh] flex justify-center">
+      <div className="w-[80%] pt-[60px]">
+        <Swiper
+          loop={true}
+          navigation
+          autoplay={{
+            delay: 4000,
+          }}
+          spaceBetween={10}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Autoplay, Navigation]}
+          slidesPerView={1}
+        >
+          {items.map((item: any, idx: number) => (
+            <SwiperSlide key={item.id} onClick={() => handleOpen(idx)}>
+              <img src={item.src} alt={item.alt} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          center
+          showCloseIcon={false}
+          classNames={{
+            overlay: "customOverlay",
+            modal: "customModal",
+            modalAnimationIn: 'customEnterModalAnimation',
+            // modalAnimationOut: 'customLeaveModalAnimation',
+          }}
+          animationDuration={800}
+        >
+          <Swiper
+            initialSlide={index}
+            spaceBetween={50}
+            navigation
+            effect="fade"
+            loop={true}
+            modules={[Navigation, EffectFade]}
+            slidesPerView={1}
+          >
+            {items.map((item: any) => (
+              <SwiperSlide key={item.id}>
+                <img src={item.src} alt={item.alt} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Modal>
+      </div>
+    </div>
+  );
+}
