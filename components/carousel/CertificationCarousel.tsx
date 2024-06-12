e0f14d408d0950cbf85223cd6bd2073a915dad77
fateh moonesi <f.moonesi@jsk.local>
2024-06-12 15:44:59 +0330

    homepage section 2 changed

--- components/carousel/CertificationCarousel.tsx
+++ components/carousel/CertificationCarousel.tsx
@@ -2,12 +2,13 @@
 import React, { useState } from "react";
 import { Autoplay, Navigation, EffectFade } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Link from "next/link";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
 import Modal from "react-responsive-modal";
-import 'react-responsive-modal/styles.css';
+import "react-responsive-modal/styles.css";
 
 const items = [
   {
@@ -35,7 +36,6 @@ const items = [
 export function CertificationsCarousel() {
   const [open, setOpen] = useState(false);
   const [index, setIndex] = useState(0);
-  //   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   const handleOpen = (selectedIndex: number) => {
     setIndex(selectedIndex);
@@ -45,69 +45,103 @@ export function CertificationsCarousel() {
   const handleClose = () => setOpen(false);
 
   return (
-    <div className="min-h-[100vh] flex justify-center">
-      <div className="w-[80%] pt-[60px]">
-        <Swiper
-          loop={true}
-          navigation
-          autoplay={{
-            delay: 4000,
-          }}
-          spaceBetween={10}
-          breakpoints={{
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 3,
-              spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 50,
-            },
-          }}
-          modules={[Autoplay, Navigation]}
-          slidesPerView={1}
+    <>
+      <div
+        className="relative bg-projects-pattern pt-[80px] lg:pt-[260px] 
+        lg:pb-[10px] bg-[top_right] bg-no-repeat bg-fixed
+        before:absolute before:content-[''] before:left-0 before:top-0
+        before:w-full before:h-full before:opacity-0 before:z-[-1]"
+      >
+        <nav
+          aria-label="breadcrumb"
+          className="px-[15px] mx-auto md:max-w-[720px] lg:max-w-[920px] relative"
         >
-          {items.map((item: any, idx: number) => (
-            <SwiperSlide key={item.id} onClick={() => handleOpen(idx)}>
-              <img src={item.src} alt={item.alt} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        <Modal
-          open={open}
-          onClose={handleClose}
-          center
-          showCloseIcon={false}
-          classNames={{
-            overlay: "customOverlay",
-            modal: "customModal",
-            modalAnimationIn: 'customEnterModalAnimation',
-            // modalAnimationOut: 'customLeaveModalAnimation',
-          }}
-          animationDuration={800}
-        >
+          <ol className="flex flex-wrap items-center absolute bottom-[20px] rounded-md bg-opacity-60 px-[1rem]">
+            <li className="flex items-center text-lg font-iransans antialiased font-normal leading-normal transition-colors duration-300 cursor-pointer text-blue-gray-900 hover:text-[#ffa500]">
+              <Link href="/" className="opacity-60">
+                خانه
+              </Link>
+              <span className="mx-2 font-sans text-lg antialiased font-normal leading-normal pointer-events-none select-none text-blue-gray-500">
+                /
+              </span>
+            </li>
+            <li className="flex items-center font-iransans text-lg antialiased font-normal leading-normal text-blue-gray-900">
+              <span>گواهینامه ها و جوایز</span>
+            </li>
+          </ol>
+        </nav>
+        <h1 className="text-[35px] font-bold absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+         گواهینامه ها 
+        </h1>
+      </div>
+      <div className="flex justify-center my-16">
+        <div className="w-[80%] pt-[60px]">
           <Swiper
-            initialSlide={index}
-            spaceBetween={50}
-            navigation
-            effect="fade"
             loop={true}
-            modules={[Navigation, EffectFade]}
+            navigation
+            autoplay={{
+              delay: 4000,
+            }}
+            spaceBetween={10}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+              },
+            }}
+            modules={[Autoplay, Navigation]}
             slidesPerView={1}
           >
-            {items.map((item: any) => (
-              <SwiperSlide key={item.id}>
+            {items?.map((item: any, idx: number) => (
+              <SwiperSlide
+                key={item.id}
+                className="cursor-pointer"
+                onClick={() => handleOpen(idx)}
+              >
                 <img src={item.src} alt={item.alt} />
               </SwiperSlide>
             ))}
           </Swiper>
-        </Modal>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            center
+            showCloseIcon={false}
+            classNames={{
+              overlay: "customOverlay",
+              modal: "customModal",
+              modalAnimationIn: "customEnterModalAnimation",
+              // modalAnimationOut: 'customLeaveModalAnimation',
+            }}
+            animationDuration={800}
+          >
+            <Swiper
+              initialSlide={index}
+              spaceBetween={50}
+              navigation
+              effect="fade"
+              loop={true}
+              modules={[Navigation, EffectFade]}
+              slidesPerView={1}
+            >
+              {items.map((item: any) => (
+                <SwiperSlide key={item.id}>
+                  <img src={item.src} alt={item.alt} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Modal>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- components/hero.js
+++ components/hero.js
@@ -1,35 +1,109 @@
+"use client";
 import React from "react";
+import pic1 from "../public/images/pic1.jpg";
+import pic2 from "../public/images/pic2.jpg";
+import pic3 from "../public/images/pic3.jpg";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
-    <section className="w-full h-screen z-8">
-      <div className="flex flex-col items-center justify-center w-full h-full gap-6">
-        <h1 className="text-center text-7xl max-w-prose">
-          Design tools
-          <br /> from the future.
-        </h1>
-        <h2 className="text-center">
-          Unleash your creativity with Diagrams <br />
-          AI-powered design tools.
-        </h2>
-        <button className="flex items-center justify-center gap-2 px-4 py-2 rounded-full transparentContainer">
-          <svg
-            className="rotate-180"
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+    <section className="w-full my-20">
+      <div className="grid grid-cols-1 lg:grid-cols-2 w-[90%] mx-auto gap-10">
+        <div className="order-2 lg:order-1">
+          <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
+            و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای
+            زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و
+            متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان
+            رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد
+            کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه
+            راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل
+            حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود
+            طراحی اساسا مورد استفاده قرار گیرد.
+          </p>
+        </div>
+        <div className="relative pr-[80px] order-1 lg:order-2">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.8, delay: 0.2, ease: "easeIn" }}
+            variants={{
+              visible: { opacity: 1, y: 0 },
+              hidden: { opacity: 0, y: 100 },
+            }}
+            className="w-[75%]"
           >
-            <path
-              d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM7.49988 1.82689C4.36688 1.8269 1.82707 4.36672 1.82707 7.49972C1.82707 10.6327 4.36688 13.1725 7.49988 13.1726V1.82689Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          Explore the future
-        </button>
+            <motion.img
+              src={pic1.src}
+              alt="pic1"
+              className="border-[5px] border-solid border-[#fff] max-w-full"
+              animate={{
+                translateX: [0, 10, 5, 10, 0],
+                translateY: [0, 10, 5, -5, 0],
+              }}
+              transition={{
+                duration: 6,
+                ease: "linear",
+                repeat: Infinity,
+              }}
+            />
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.8, delay: 0.4, ease: "easeIn" }}
+            variants={{
+              visible: { opacity: 1, y: 0 },
+              hidden: { opacity: 0, y: 100 },
+            }}
+            className="w-[75%] absolute right-0 top-[25%]"
+          >
+            <motion.img
+              src={pic2.src}
+              alt="pic2"
+              className="border-[5px] border-solid border-[#fff] max-w-full"
+              animate={{
+                translateX: [0, 10, 5, 10, 0],
+                translateY: [0, 10, 5, -5, 0],
+              }}
+              transition={{
+                duration: 8,
+                ease: "linear",
+                repeat: Infinity,
+              }}
+            />
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.8, delay: 0.6, ease: "easeIn" }}
+            variants={{
+              visible: { opacity: 1, y: 0 },
+              hidden: { opacity: 0, y: 100 },
+            }}
+            className="relative z-[1] mt-[-15%] ml-[-20%] w-[90%]"
+          >
+            <motion.img
+              src={pic3.src}
+              alt="pic3"
+              className="border-[5px] border-solid border-[#fff] max-w-full"
+              animate={{
+                translateX: [0, 10, 5, 10, 0],
+                translateY: [0, 10, 5, -5, 0],
+              }}
+              transition={{
+                duration: 10,
+                ease: "linear",
+                repeat: Infinity,
+              }}
+            />
+          </motion.div>
+        </div>
       </div>
     </section>
   );
