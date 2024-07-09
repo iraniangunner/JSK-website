8109bf0f3c60707365fcedf1882340f396f7825f
fateh moonesi <f.moonesi@jsk.local>
2024-07-09 11:32:44 +0330

    some loading added

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
       <MainCarousel data={dataSlider} />
       <ServicesSection />
       <section
-        className="relative min-h-[500px] bg-section-2-pattern bg-cover overflow-hidden 
+        className="relative min-h-[500px] bg-section-2-pattern bg-cover overflow-hidden flex justify-center items-center
         bg-no-repeat bg-center before:content-[''] before:absolute before:left-0 before:top-0
         before:w-full before:h-full before:opacity-[0.9] before:z-[0] before:bg-[#042038]"
       >
