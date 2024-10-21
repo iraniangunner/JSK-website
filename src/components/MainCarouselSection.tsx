3f02c3eb994625d031b29feb3d0d8c668455edc2
fateh moonesi <f.moonesi@jsk.local>
2024-10-21 17:51:16 +0330

    slider error changed

--- src/components/MainCarouselSection.tsx
+++ src/components/MainCarouselSection.tsx
@@ -11,10 +11,10 @@ export async function MainCarouselSection() {
   } catch (error) {
     return (
       <div className="mx-auto max-w-screen-sm text-center z-[1] relative">
-        <div className="mb-4 text-7xl flex justify-center items-center font-extrabold tracking-tight text-white lg:text-9xl">
-          <CgDanger color="white" />
+        <div className="mb-4 text-7xl flex justify-center items-center font-extrabold tracking-tight lg:text-9xl">
+          <CgDanger color="red" />
         </div>
-        <p className="mb-4 text-lg font-light text-white">
+        <p className="mb-4 text-lg font-light">
           مشکلی پیش آمده دوباره تلاش کنید
         </p>
       </div>
