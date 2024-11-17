0532238f53ee3fbdeb3b289540685004af0f0744
fateh moonesi <f.moonesi@jsk.local>
2024-11-17 16:50:15 +0330

    top loader added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 
-const iranSans = localFont({ src: "../../public/fonts/IRANSansX-Regular.woff" });
+const iranSans = localFont({
+  src: "../../public/fonts/IRANSansX-Regular.woff",
+});
 
 export const metadata: Metadata = {
   title: "ژیوار صنعت کیان | صفحه اصلی",
@@ -29,6 +32,17 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <body className={iranSans.className}>
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
         <Header />
         {children}
         <Footer />
