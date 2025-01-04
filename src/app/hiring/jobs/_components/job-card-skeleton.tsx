4764d1b96887bf757731929fc96173aa8eedd208
fateh moonesi <f.moonesi@jsk.local>
2025-01-04 17:24:57 +0330

    job card skeleton changed

--- src/app/hiring/jobs/_components/job-card-skeleton.tsx
+++ src/app/hiring/jobs/_components/job-card-skeleton.tsx
@@ -1,13 +1,17 @@
 export function JobCardSkeleton() {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 animate-pulse">
-      <div className="flex justify-between items-start">
-        <div className="h-6 bg-gray-200 rounded w-3/4"></div>
-        <div className="h-5 bg-gray-200 rounded w-20"></div>
-      </div>
-      <div className="mt-2 flex items-center">
-        <div className="h-4 w-4 bg-gray-200 rounded-full ml-2"></div>
-        <div className="h-4 bg-gray-200 rounded w-1/3"></div>
+    <div className="bg-white rounded-lg shadow-md animate-pulse">
+      <div className="p-6">
+        <div className="flex justify-between items-start">
+          <div className="h-6 w-32 bg-gray-300 rounded animate-pulse"></div>
+        </div>
+        <div className="mt-2 flex items-center text-gray-600">
+          <div className="h-4 w-4 bg-gray-300 rounded-full animate-pulse"></div>
+          <div className="h-4 w-20 bg-gray-300 rounded ml-2 animate-pulse"></div>
+        </div>
+        <div className="flex justify-center items-center w-[80%] xl:w-[40%] px-2 py-2 mt-8 bg-gray-300 animate-pulse rounded">
+          &nbsp;
+        </div>
       </div>
     </div>
   );
