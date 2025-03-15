8bc06732139bf7c674c41bd8ba0426eed756ee12
fateh moonesi <f.moonesi@jsk.local>
2025-03-15 16:47:57 +0330

    robots changed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,14 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://jsk-co.com';
   return {
     rules: {
-      userAgent: "*",
-      allow: "/",
-      //   disallow: '/private/',
+      userAgent: '*',
+      allow: '/',
+      // disallow: ['/api/', '/admin/'],
     },
-    sitemap: "https://jsk-co.com/sitemap.xml",
+    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const locales = ["en", "fa"] as const
-export type Locale = (typeof locales)[number]
+export const locales = ["fa", "en"] as const;
+export type Locale = (typeof locales)[number];
